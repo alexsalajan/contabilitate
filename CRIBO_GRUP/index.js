ed2a32d9dae480d786b8c69664c98ab020b4f514
Alexandru Salajan <alex.salajan@gmail.com>
2022-08-02 14:51:42 +0300

    GRIBO GRUP

--- CRIBO_GRUP/index.js
+++ CRIBO_GRUP/index.js
@@ -4,24 +4,7 @@ node index.js
 ************************** */
 
 const fs = require('fs');
-const readXlsxFile = require('read-excel-file/node');
-const moment = require('moment');
 const { xml2json, js2xml } = require('xml-js');
-
-// Date furnizor
-const FurnizorNume = 'TIPOGRAFIA 3B S.R.L.';
-const FurnizorCIF = '16203419';
-const FurnizorNrRegCom = 'J40/3451/2004';
-const FurnizorCapital = '1700';
-const FurnizorTara = 'Romania';
-const FurnizorJudet = 'B';
-const FurnizorAdresa = 'str. Zamora nr. 1, sector 4';
-
-const CONT_MARFA = '707';
-const CONT_PRODUSE = '7015';
-
-let dataPrimeiFacturi = '';
-let numarulPrimeiFacturi = '';
 
 const content = xml2json(
     fs.readFileSync('input.xml', 'utf8'),
@@ -29,7 +12,17 @@ const content = xml2json(
 );
 
 const facturi = JSON.parse(content)['Facturi']['Factura'];
-console.log(facturi);
+
+const FurnizorCIF = 'RO15438697'
+let dataPrimeiFacturi = '';
+let numarulPrimeiFacturi = '';
+facturi.forEach(factura => {
+    if (!!factura?.['Antet']?.['ClientLocalitate']?.['_text']) {
+        factura['Antet']['ClientLocalitate']['_text'] = factura['Antet']['ClientLocalitate']['_text'].toUpperCase().replace(/SECTOR(UL)?/, 'BUCURESTI SECTOR');
+    }
+    dataPrimeiFacturi = dataPrimeiFacturi || factura['Antet']['FacturaData']['_text'];
+    numarulPrimeiFacturi = numarulPrimeiFacturi || factura['Antet']['FacturaNumar']['_text'];
+});
 
 const xml = js2xml({
     _declaration: { _attributes: { version: '1.0' } },
@@ -37,7 +30,7 @@ const xml = js2xml({
         Factura: facturi
     }
 }, { compact: true, ignoreComment: true, spaces: 4 });
-fs.writeFile(`./output.xml`, xml, (err) => {
+fs.writeFile(`./F_${FurnizorCIF}_${numarulPrimeiFacturi}_${dataPrimeiFacturi}.xml`, xml, (err) => {
     if (err) return console.log(err);
     console.log('File successfully processed!!!');
 });
